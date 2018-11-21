7722be2a75ce5d36b4a96ccc4ea4737c3a4c3b94
yczhang1017 <zhangyuchen12344@gmail.com>
2018-11-21 12:31:29 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -359,8 +359,8 @@ def main():
     if torch.cuda.is_available():
         model = model.cuda()
     
-    criterion = FocalLoss(gamma=1,pos_weight=pos_weight)
-    #criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+    #criterion = FocalLoss(gamma=1,pos_weight=pos_weight)
+    criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
     optimizer = optim.SGD(model.parameters(),lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=0.3)
     t00 = time.time()
@@ -427,13 +427,13 @@ def main():
                     print('{} L: {:.4f} p: {:.4f} r: {:.4f} F1: {:.4f} Time: {:.4f}s'.format(
                             num,average_loss,average_prec,average_recall,average_F1,t02-t01))
             
-            class_prec=correct_class.double()/selected_class.double()*100
-            class_recall=correct_class.double()/relevant_class.double()*100
+            class_prec=(correct_class.double()/selected_class.double()*100).cup().numpy()
+            class_recall=(correct_class.double()/relevant_class.double()*100).cup().numpy()
             print(phase)
-            print('c:'+'\t'.join('{:4d}'.format(i) for i in range(NLABEL)))
+            print('c:'+''.join('{:4d}'.format(i) for i in range(NLABEL)))
             #print('n:'+'\t'.join('{:4d}'.format(i) for i in correct_class.numpy()))
-            print('p:'+'\t'.join('{:4.0f}'.format(i) for i in class_prec.numpy()))
-            print('r:'+'\t'.join('{:4.0f}'.format(i) for i in class_recall.numpy()))
+            print('p:'+''.join('{:4.0f}'.format(i) for i in class_prec))
+            print('r:'+''.join('{:4.0f}'.format(i) for i in class_recall))
             
             
             if phase == 'val' and average_F1 > best_F1:
