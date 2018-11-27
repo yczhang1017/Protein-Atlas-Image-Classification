019c7b8c2177d181637120a430bcc5062632c239
yczhang1017 <zhangyuchen12344@gmail.com>
2018-11-27 01:02:37 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -98,11 +98,11 @@ mean=[0.054813755064775954, 0.0808928726780973, 0.08367144133595689, 0.052260835
 std=[0.15201123862047256, 0.14087982537762958, 0.139965362113942, 0.10123220339551285]
 transform['train']=transforms.Compose(
     [
-     transforms.RandomAffine(20,scale=(1,6),shear=20,resample=PIL.Image.BILINEAR),
-     #transforms.RandomResizedCrop(512),
+     transforms.RandomAffine(20,shear=20,resample=PIL.Image.BILINEAR),
+     #transforms.RandomRotation(20),
+     transforms.RandomResizedCrop(512, scale=(0.2, 1.0)),
      transforms.RandomHorizontalFlip(),
      transforms.RandomVerticalFlip(),
-     #transforms.RandomRotation(20),
      transforms.ToTensor(),
      transforms.Normalize(mean,std)
      ])
