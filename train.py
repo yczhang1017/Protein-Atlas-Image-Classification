60eafe7e8ca50b069bfa8df5a432047f71478424
yczhang1017 <zhangyuchen12344@gmail.com>
2018-12-04 19:22:46 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ parser.add_argument('--workers', default=4, type=int,
                     help='Number of workers used in dataloading')
 parser.add_argument('--cuda', default=True, type=str2bool,
                     help='Use CUDA to train model')
-parser.add_argument('--lr', '--learning-rate', default=4e-2, type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     help='initial learning rate')
 parser.add_argument('--epochs', default=50, type=int,
                     help='number of epochs to train')
