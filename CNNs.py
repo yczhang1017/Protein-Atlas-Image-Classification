449ab82e10bffc7b68ad18ab4051bebf081b415d
yczhang1017 <zhangyuchen12344@gmail.com>
2018-12-06 19:28:24 -0500

    Add files via upload

--- CNNs.py
+++ CNNs.py
@@ -2,11 +2,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-from torchvision.models.vgg import make_layers
 NLABEL=28
 '''
 Resnet
 '''
+def vgg_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 4
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
 
 class VGG(nn.Module):
     def __init__(self, features, num_classes=NLABEL, init_weights=True):
@@ -365,7 +378,7 @@ def CNN_models(model_type):
         model_url=inception_url['inception_v3_google']
         con1_name='Conv2d_1a_3x3.conv.weight'
     elif model_type=='vgg16':
-        model =VGG(make_layers([64, 64, 'M', 128, 128, 'M',
+        model =VGG(vgg_layers([64, 64, 'M', 128, 128, 'M',
                 256, 256, 256, 'M', 512, 512, 'M', 
                 512, 512,'M',512, 512, 'M'], batch_norm=False))
         model_url=vgg_url['vgg16']
