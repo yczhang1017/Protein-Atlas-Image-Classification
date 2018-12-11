f30a06dfa19e32825fc692ccac604ce283528bbc
yczhang1017 <zhangyuchen12344@gmail.com>
2018-12-11 12:54:46 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -219,19 +219,28 @@ class F1Loss(nn.Module):
         tp = torch.sum(y_true*y_pred,1)
         fp = torch.sum(y_pred,1)
         fn = torch.sum(y_true,1)
-        p=torch.zeros_like(tp)
-        r=torch.zeros_like(tp)
         #F1=torch.zeros_like(tp)
+        
         epsilon=1e-8
-        p[tp>epsilon]=tp[tp>epsilon]/fp[tp>epsilon]
-        r[tp>epsilon]=tp[tp>epsilon]/fn[tp>epsilon]
-        mp=torch.mean(p)
-        mr=torch.mean(r)
+        if 1==0:
+            p=torch.zeros_like(tp)
+            r=torch.zeros_like(tp)
+            p[tp>epsilon]=tp[tp>epsilon]/fp[tp>epsilon]
+            r[tp>epsilon]=tp[tp>epsilon]/fn[tp>epsilon]
+            mp=torch.mean(p)
+            mr=torch.mean(r)
+            mF1=2*mp*mr/(mp+mr)
+        if 1==1:
+            prev=torch.zeros_like(tp)
+            rrev=torch.zeros_like(tp)
+            F1=torch.zeros_like(tp)
+            prev[tp>epsilon]=p[tp>epsilon]/tp[tp>epsilon]
+            rrev[tp>epsilon]=fn[tp>epsilon]/tp[tp>epsilon]
+            F1[tp>epsilon]=2/(prev[tp>epsilon]+rrev[tp>epsilon])
+            mF1=torch.mean(F1)
         #p=tp/(fp+epsilon)
         #r=tp/(fn+epsilon)
         #f1_res = 2*fp/(tp+ epsilon) + 2*fn/(tp+ epsilon)
-        #F1[tp>epsilon]=2/(prev[tp>epsilon]+rrev[tp>epsilon])
-        mF1=2*mp*mr/(mp+mr)
         return 1-mF1
 
  
